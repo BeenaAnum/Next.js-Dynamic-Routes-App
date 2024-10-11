3ce3f84aa54fb568fdd017f1c37d8ea807ce2df6
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-11 13:30:11 +0500

    Update page.tsx

--- country_app/app/page.tsx
+++ country_app/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className="flex justify-center items-center text-black"><b>Click here for Country List</b></h1><br />
      <div className="flex justify-center items-center">
      <button className="bg-black border-yellow-300 border-2 p-1 rounded-md hover:text-red-500">
-      <Link href="./country">show Country list</Link></button>
+      <Link className="font-bold" href="./country">show Country list</Link></button>
      </div> 
 
     </div>
