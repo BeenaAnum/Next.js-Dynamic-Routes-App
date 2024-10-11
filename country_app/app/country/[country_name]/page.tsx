f190a16edb5e0b76895e7a21934eb5279dcb1efc
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-11 13:20:12 +0500

    Add files via upload

--- country_app/app/country/[country_name]/page.tsx
+++ country_app/app/country/[country_name]/page.tsx
@@ -18,24 +18,24 @@ export default function CountryDetails({ params }:{params :{country_name:string}
 
   if (!country) {
     // Handle if country not found
-    return <div><h1 className="flex justify-center items-center m-2"><b>Country not found</b></h1>
+    return (<div><h1 className="flex justify-center items-center m-2"><b>Country not found</b></h1>
      <div className="flex justify-center items-center">
-      <button className="bg-black border-yellow-300 border-2 p-1 m-5 rounded-md">
-      <Link href="/country">Back to Country List</Link></button>
+      <button className="bg-black border-yellow-300 border-2 p-1 m-5 rounded-md ">
+      <Link className="hover:text-red-500" href="/country">Back to Country List</Link></button>
      </div>
-  </div>;
-    
+    </div>
+  );
   }
 
   return (
     <div>
-      <h1 className="flex justify-center items-center">Country: {country.name}</h1>
-      <p className="flex justify-center items-center">Population: {country.population}</p>
-      <p className="flex justify-center items-center">Capital: {country.capital}</p>
-    <div className="flex justify-center items-center">
-      <button className="bg-black  border-yellow-300 border-2 p-1 m-2 rounded-md">
-      <Link href="/country">Back to Country List</Link></button>
-    </div>
+      <h1 className="flex justify-center items-center font-bold">Country: {country.name}</h1>
+      <p className="flex justify-center items-center font-bold">Population: {country.population}</p>
+      <p className="flex justify-center items-center font-bold">Capital: {country.capital}</p>
+     <div className="flex justify-center items-center font-bold">
+      <button className="bg-black border-yellow-300 border-2 p-1 m-2 rounded-md">
+      <Link className="hover:text-red-500 font-bold" href="/country">Back to Country List</Link></button>
+     </div>
     </div>
   );
 }
