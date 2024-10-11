46187fc43fc9c916567e1bbf01caf43c5fc66c8a
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-11 13:26:20 +0500

    Update Footer.tsx

--- country_app/app/components/Footer.tsx
+++ country_app/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin ,FaG } from "react-icons/fa6";
 
 export default function Footer() {
     return (
-      <footer className="bg-black border-2 border-yellow-300 text-yellow p-1 w-full fixed  bottom-0  ">
+      <footer className="bg-black border-2 border-yellow-300 text-yellow font-bold p-1 w-full fixed bottom-0  ">
         <div className="flex justify-center items-center ">
           <p>&copy; 2024 My Country App. All rights reserved.</p> 
           <a href="https://www.linkedin.com/in/beena-anum-2b2963254" target="_blank">
